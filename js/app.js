c580cdfa59455f75a45856c4e938ccf3d5b66991
FJ9019 <jfenelus@jfenelus.com>
2022-09-01 10:27:11 -0400

    Services Change

--- js/app.js
+++ js/app.js
@@ -61,5 +61,19 @@ String.prototype.includes||(String.prototype.includes=function(e,a){"use strict"
                   });
                 
                 
-                
-             
+            
+                  function myFunction() {
+                    var dots = document.getElementById("dots");
+                    var moreText = document.getElementById("more");
+                    var btnText = document.getElementById("myBtn");
+                  
+                    if (dots.style.display === "none") {
+                      dots.style.display = "inline";
+                      btnText.innerHTML = "Read more";
+                      moreText.style.display = "none";
+                    } else {
+                      dots.style.display = "none";
+                      btnText.innerHTML = "Read less";
+                      moreText.style.display = "inline";
+                    }
+                  }                  
