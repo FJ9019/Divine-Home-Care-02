9e7e1b23a1391b5fcd3d2d8abb9a0e69dea8c2b5
FJ9019 <jfenelus@jfenelus.com>
2022-09-02 14:14:58 -0400

    Change In Services Page

--- js/app.js
+++ js/app.js
@@ -51,29 +51,24 @@ String.prototype.includes||(String.prototype.includes=function(e,a){"use strict"
 
 
 
+                
 
-                document.querySelector('#contact-form').addEventListener('submit', (e) => {
-                    e.preventDefault();
-                    e.target.elements.name.value = '';
-                    e.target.elements.subject.value = '';
-                    e.target.elements.email.value = '';
-                    e.target.elements.message.value = '';
-                  });
-                
-                
-            
-                  function myFunction() {
-                    var dots = document.getElementById("dots");
-                    var moreText = document.getElementById("more");
-                    var btnText = document.getElementById("myBtn");
-                  
-                    if (dots.style.display === "none") {
-                      dots.style.display = "inline";
-                      btnText.innerHTML = "Read more";
-                      moreText.style.display = "none";
-                    } else {
-                      dots.style.display = "none";
-                      btnText.innerHTML = "Read less";
-                      moreText.style.display = "inline";
-                    }
-                  }                  
+const parentContainer = document.querySelector('.read-more-container');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read Less..." : "Read More...";
+})
+
+
